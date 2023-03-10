14fc9838dc22b904f2a92a5d531e925a7f59b96f
bot209 <bot2014cs@gmail.com>
2023-03-10 19:40:11 +0300

    Добавить firefox конфигурацию

--- conftest.py
+++ conftest.py
@@ -1,12 +1,28 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService 
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
-@pytest.fixture(scope='function')
-def browser():
-    print('\nЗапускаю браузер')
-    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    browser.maximize_window()
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+
+    if browser_name == "chrome":
+        print("\n Запускаю браузер 'chrome'")
+        browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+    elif browser_name == "firefox":
+        print("\n Запускаю браузер 'firefox'")
+        browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
     yield browser
-    print('\nЗакрываю браузер')
+    print("\n Закрываю браузер")
+    browser.quit()
